92ba6c9e7d404df4e04de799ed70548f5cf01f0f
James Xie <jxie418@gmail.com>
2015-12-23 11:05:59 -0800

    Add Token

--- app.js
+++ app.js
@@ -1,60 +1,58 @@
-var express = require('express');
+/*
+ Load Twilio configuration from .env config file - the following environment
+ variables should be set:
+ process.env.TWILIO_ACCOUNT_SID
+ process.env.TWILIO_API_KEY
+ process.env.TWILIO_API_SECRET
+ process.env.TWILIO_CONFIGURATION_SID
+ */
+var http = require('http');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var AccessToken = require('twilio').AccessToken;
+var ConversationsGrant = AccessToken.ConversationsGrant;
+var express = require('express');
+var randomUsername = require('./randos');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+// Create Express webapp
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+/*
+ Generate an Access Token for a chat application user - it generates a random
+ username for the client requesting a token, and takes a device ID as a query
+ parameter.
+ */
+app.get('/token', function(request, response) {
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  var identity = (typeof (request.query.device) !='undefined' && request.query.device !='') ? request.query.device: randomUsername();
+  console.log("Device is:" + request.query.device);
+
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created
+  var token = new AccessToken(
+      "AC8ae1c772561cb2c33f75154e269d0938",
+      "SK38697f06ee988cfd4181f836e60c8904",
+      "PNquUbHDJpLMTs1HiOYEZMlMZdXU5VUC"
+  );
+
+  // Assign the generated identity to the token
+  token.identity = identity;
+
+  //grant the access token Twilio Video capabilities
+  var grant = new ConversationsGrant();
+  grant.configurationProfileSid = "VSc7b98796c0f90904cc650a40510623ab";
+  token.addGrant(grant);
+
+  // Serialize the token to a JWT string and include it in a JSON response
+  response.send({
+    identity: identity,
+    token: token.toJwt()
+  });
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+// Create http server and run it
+var server = http.createServer(app);
+var port = 3009;
+server.listen(port, function() {
+  console.log('Express server running on *:' + port);
 });
-
-
-module.exports = app;
